f2c6b3a3014bf48417ea1deec3ec982273fcb0ed
itblock <wnsuddlsla@gmail.com>
2018-12-14 14:31:44 +0900

    Fixed R markdown path issue and fix #8

--- R/drug_era_data.R
+++ R/drug_era_data.R
@@ -108,8 +108,7 @@ tryCatch({
   GROUP BY gap_day_range
 >>>>>>> 838f8f9... GEMINI for package
 )
-SELECT gap_day_range, ROUND((person_count/CONVERT(float,(SELECT SUM(person_count) FROM T1)))*100,1) as person_ratio FROM T1
-ORDER BY CAST(LEFT(gap_day_range,CHARINDEX('~',gap_day_range)-1)AS INT) + CAST(RIGHT(gap_day_range,CHARINDEX('~',gap_day_range)-1)AS INT) ASC"
+SELECT gap_day_range, ROUND((person_count/CONVERT(float,(SELECT SUM(person_count) FROM T1)))*100,1) as person_ratio FROM T1"
 drug_eratbl_gap_days <<- queryRender(sql)
 # sql <- SqlRender::renderSql(sql, cdm_database_schema = cdmDatabaseSchema)$sql
   # sql <- SqlRender::translateSql(sql, targetDialect = attr(connection, "dbms"))$sql
